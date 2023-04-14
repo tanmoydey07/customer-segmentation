0eb1640a2f301a1ae8e4357b66bba309bd333d9e
Tanmoy Dey <118444397+tanmoydey07@users.noreply.github.com>
2023-04-14 12:11:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import pandas as pd
 from PIL import Image
 
 ## loading the kmeans model
-model = load_model('/modular_code/output/Final_kmeans_model')
+model = load_model('Final_Kmeans_Model')
 
 ## defining a function to make predictions
 def predict(model, input_df):
